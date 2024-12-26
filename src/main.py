edcb7dac76c4c516e2a4cab0fd5f9cb207798479
root <root@DESKTOP-FPRDUFK.>
2024-12-25 21:28:34 -0600

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+def main():
+    dummy_url = TextNode("This is a text node", TextType.ITALIC)
+    print(dummy_url)
+
+
+
+
+if __name__ == '__main__':
+    main()
