32ae7d0733183964cf75f0ed88f9c1ea8920b280
root <root@DESKTOP-FPRDUFK.>
2024-12-28 09:17:29 -0600

    add split_nodes_delimiter and unittests

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,25 @@
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
+def split_notes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            delimiter_count = len(node.text.split(delimiter))-1
+            if delimiter_count % 2 != 0:
+                raise Exception(f"Unpaired delimiter {delimiter}")
+            split_text = node.text.split(delimiter)
+            for i in range(len(split_text)):
+                if i % 2 == 0:
+                    result.append(TextNode(split_text[i], TextType.TEXT))
+                else:
+                    result.append(TextNode(split_text[i], text_type))
+        else:
+            result.append(node)
+    return(result)
+        
+
+
+node = TextNode("This is `code` and this is *italic*.", TextType.TEXT)
+result = split_notes_delimiter([node], "`", TextType.CODE)
+result2= split_notes_delimiter(result, "*", TextType.ITALIC)
+print(result2)
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,41 @@
+import unittest
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from split_nodes_delimiter import split_notes_delimiter
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_no_delimiter(self):
+        node = TextNode("Simple text", TextType.TEXT)
+        result = split_notes_delimiter([node], "`", TextType.CODE)
+        expected = [TextNode("Simple text", TextType.TEXT)]
+        self.assertEqual(result, expected)
+
+    def test_single_pair_delimiter(self):
+        node = TextNode("This is `code`.", TextType.TEXT)
+        result = split_notes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(".", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
+
+    def test_unpaired_delimiter(self):
+        node = TextNode("This is `unclosed.", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_notes_delimiter([node], "`", TextType.CODE)
+
+    def test_complex_text(self):
+        node = TextNode("This is `code` and this is *italic*.", TextType.TEXT)
+        result = split_notes_delimiter([node], "`", TextType.CODE)
+        result = split_notes_delimiter(result, "*", TextType.ITALIC)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" and this is ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(".", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
